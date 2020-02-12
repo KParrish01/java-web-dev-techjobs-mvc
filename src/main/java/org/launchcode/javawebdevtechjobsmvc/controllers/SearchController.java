e919ecc426a4003e8a9a71f7158b3557a1e65bdf
KParrish01 <klaraparrish@yahoo.com>
2020-02-12 17:12:10 -0600

    Part of TODO 3 done

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
@@ -25,4 +26,20 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+    @RequestMapping(value="results")
+//    @PostMapping(value="results")  // <<<which is better?
+    public String displaySearchResults(Model model, @RequestParam String searchType, String searchTerm){
+        /* do whatever 3) 1 et al recommend ******* also check variable name syntax against templates******* */
+
+        ArrayList<Job> jobs;
+         if (searchType.toLowerCase().equals("all")) {  //
+             jobs = JobData.findAll();
+//             model.addAttribute(("title","All Jobs"); // <<<<<< ******** what is wrong with this? *******
+         } else {
+             jobs = JobData.findByColumnAndValue(searchType, searchTerm); // <<<<****** check if this is correct! *******
+             model.addAttribute("title", "Jobs with " + ListController.columnChoices.get(searchType) + ": " + searchTerm);
+         }
+        model.addAttribute("jobs", jobs);
+        return "search";
+    }
 }
