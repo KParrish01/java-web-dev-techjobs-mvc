d6b13f9b74d686314a8a1c57a304717e24210988
KParrish01 <klaraparrish@yahoo.com>
2020-02-13 19:05:47 -0600

    Assignment donegit add .! (But only part of Bonus so far.)

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -21,6 +21,12 @@ public class SearchController {
     @RequestMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
+//        model.addAttribute("tableChoices", tableChoices);
+        model.addAttribute("employers", JobData.getAllEmployers());
+        model.addAttribute("locations", JobData.getAllLocations());
+        model.addAttribute("positions", JobData.getAllPositionTypes());
+        model.addAttribute("skills", JobData.getAllCoreCompetency());
+        model.addAttribute("all",JobData.findAll());     // added for step 2) *******
         return "search";
     }
 
@@ -28,18 +34,40 @@ public class SearchController {
 
     @RequestMapping(value="results")
 //    @PostMapping(value="results")  // <<<which is better?
-    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam  String searchTerm){
+    public String displaySearchResults(Model model, @RequestParam(defaultValue = "all") String searchType, @RequestParam  String searchTerm){
 
         ArrayList<Job> jobs;
-         if (searchType.toLowerCase().equals("all")) {  //
+         if (searchType.toLowerCase().equals("all") && (searchTerm.toLowerCase().equals("all") || searchTerm.toLowerCase().equals(" ") || searchTerm.toLowerCase().equals("") || searchTerm.toLowerCase().equals(null))) {  //
              jobs = JobData.findAll();
-             model.addAttribute("title","All Jobs"); // <<<<<< ******** what is wrong with this? *******
+             model.addAttribute("title","All Jobs");
+//         } else if (searchType.toLowerCase().equals("all") ){
+         } if (searchType.toLowerCase().equals("all") ){
+             jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+             model.addAttribute("title",searchType + " fields searched for search term: '" + searchTerm + "'");
          } else {
-             jobs = JobData.findByColumnAndValue(searchType, searchTerm); // <<<<****** check if this is correct! *******
-             model.addAttribute("title", "Jobs with " + ListController.columnChoices.get(searchType) + ": " + searchTerm);
-         }
+             jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+             model.addAttribute("title","Jobs with " + ListController.columnChoices.get(searchType) + ": " + searchTerm);
+        }
         System.out.println("searchType: " + searchType + ", searchTerm: " + searchTerm);
         model.addAttribute("jobs", jobs);
         return "search";
     }
+
+    //The older version that did a high level search:
+//    @RequestMapping(value="results")
+////    @PostMapping(value="results")  // <<<which is better?
+//    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam  String searchTerm){
+//
+//        ArrayList<Job> jobs;
+//        if (searchType.toLowerCase().equals("all")) {  //
+//            jobs = JobData.findAll();
+//            model.addAttribute("title","All Jobs"); // <<<<<< ******** what is wrong with this? *******
+//        } else {
+//            jobs = JobData.findByColumnAndValue(searchType, searchTerm); // <<<<****** check if this is correct! *******
+//            model.addAttribute("title", "Jobs with " + ListController.columnChoices.get(searchType) + ": " + searchTerm);
+//        }
+//        System.out.println("searchType: " + searchType + ", searchTerm: " + searchTerm);
+//        model.addAttribute("jobs", jobs);
+//        return "search";
+//    }
 }
