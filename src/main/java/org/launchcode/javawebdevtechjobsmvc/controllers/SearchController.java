603b20e40ba16b8932778edd46b2d5128c2094dd
KParrish01 <klaraparrish@yahoo.com>
2020-02-14 00:38:46 -0600

    Assignment 3 incl. Bonus 2 and 3 done

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -21,21 +21,14 @@ public class SearchController {
     @RequestMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
-////        model.addAttribute("tableChoices", tableChoices);
-//        model.addAttribute("employers", JobData.getAllEmployers());
-//        model.addAttribute("locations", JobData.getAllLocations());
-//        model.addAttribute("positions", JobData.getAllPositionTypes());
-//        model.addAttribute("skills", JobData.getAllCoreCompetency());
-//        model.addAttribute("all",JobData.findAll());     // added for step 2) *******
         return "search";
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
     @RequestMapping(value="results")
-//    @PostMapping(value="results")  // <<<which is better?
+//    @PostMapping(value="results")  // <<<which is better? - PostMapping does not work for Bonus 3 links
     public String displaySearchResults(Model model, @RequestParam(defaultValue = "all") String searchType, @RequestParam  String searchTerm){
-        model.addAttribute("columns", columnChoices);
 
         ArrayList<Job> jobs;
          if (searchType.toLowerCase().equals("all") && (searchTerm.toLowerCase().equals("all") || searchTerm.toLowerCase().equals(" ") || searchTerm.toLowerCase().equals("") || searchTerm.toLowerCase().equals(null))) {  //
@@ -47,28 +40,13 @@ public class SearchController {
              model.addAttribute("title",searchType + " fields searched for search term: '" + searchTerm + "'");
          } else {
              jobs = JobData.findByColumnAndValue(searchType, searchTerm);
-             model.addAttribute("title","Jobs with " + ListController.columnChoices.get(searchType) + ": " + searchTerm);
+//             model.addAttribute("title","Jobs with " + ListController.columnChoices.get(searchType) + ": '" + searchTerm + "'");
+            model.addAttribute("title","Jobs with " + searchType + ": '" + searchTerm + "'");
         }
         System.out.println("searchType: " + searchType + ", searchTerm: " + searchTerm);
+        model.addAttribute("columns", columnChoices);
         model.addAttribute("jobs", jobs);
         return "search";
     }
 
-    //The older version that did a high level search:
-//    @RequestMapping(value="results")
-////    @PostMapping(value="results")  // <<<which is better?
-//    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam  String searchTerm){
-//
-//        ArrayList<Job> jobs;
-//        if (searchType.toLowerCase().equals("all")) {  //
-//            jobs = JobData.findAll();
-//            model.addAttribute("title","All Jobs"); // <<<<<< ******** what is wrong with this? *******
-//        } else {
-//            jobs = JobData.findByColumnAndValue(searchType, searchTerm); // <<<<****** check if this is correct! *******
-//            model.addAttribute("title", "Jobs with " + ListController.columnChoices.get(searchType) + ": " + searchTerm);
-//        }
-//        System.out.println("searchType: " + searchType + ", searchTerm: " + searchTerm);
-//        model.addAttribute("jobs", jobs);
-//        return "search";
-//    }
 }
